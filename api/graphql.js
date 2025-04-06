e6592291e5b007ba15c69419f0c97207f2dbbf19
Pmack0702 <meetpatel7026@gmail.com>
2025-04-06 11:31:08 -0700

    vercel

---
+++ api/graphql.js
@@ -0,0 +1,29 @@
+// api/graphql.js
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const connectDB = require("../db");
+const typeDefs = require("../graphql/schema");
+const resolvers = require("../graphql/resolvers");
+const { createServer } = require("http");
+const { parse } = require("url");
+
+require("dotenv").config();
+
+let serverInstance;
+
+module.exports = async (req, res) => {
+  if (!serverInstance) {
+    await connectDB(); // Connect to your DB (MongoDB)
+
+    const app = express();
+    const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app, path: "/api/graphql" });
+
+    serverInstance = app;
+  }
+
+  const parsedUrl = parse(req.url, true);
+  serverInstance(req, res);
+};
